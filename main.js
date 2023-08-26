9e336e70d410bd3f9860733ab76eae7c47cffa28
aartura <artur@Arturs-MacBook-Pro.local>
2023-08-26 16:15:20 +0200

    fortune-cookies-api

--- main.js
+++ main.js
@@ -3,81 +3,46 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-// require("dotenv").config();
 
-const serverPort = process.env.PORT || 3000;
-const server = http.createServer(app);
+const serverPort = 5000;
+
 const WebSocket = require("ws");
-
-let keepAliveId;
-
-const wss =
-  process.env.NODE_ENV === "production"
-    ? new WebSocket.Server({ server })
-    : new WebSocket.Server({ port: 5001 });
-
-server.listen(serverPort);
-console.log(`Server started on port ${serverPort} in stage ${process.env.NODE_ENV}`);
-
-wss.on("connection", function (ws, req) {
-  console.log("Connection Opened");
-  console.log("Client size: ", wss.clients.size);
-
-  if (wss.clients.size === 1) {
-    console.log("first connection. starting keepalive");
-    keepServerAlive();
-  }
-
-  ws.on("message", (data) => {
-    let stringifiedData = data.toString();
-    if (stringifiedData === 'pong') {
-      console.log('keepAlive');
-      return;
-    }
-    broadcast(ws, stringifiedData, false);
-  });
-
-  ws.on("close", (data) => {
-    console.log("closing connection");
-
-    if (wss.clients.size === 0) {
-      console.log("last client disconnected, stopping keepAlive interval");
-      clearInterval(keepAliveId);
-    }
-  });
-});
-
-// Implement broadcast function because of ws doesn't have it
-const broadcast = (ws, message, includeSelf) => {
-  if (includeSelf) {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  } else {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  }
-};
-
-/**
- * Sends a ping message to all connected clients every 50 seconds
- */
- const keepServerAlive = () => {
-  keepAliveId = setInterval(() => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send('ping');
-      }
-    });
-  }, 50000);
-};
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!!!');
+});
+
+const API = "http://yerkee.com/api/fortune";
+const wss = new WebSocket.Server(
+  {
+    port: serverPort,
+  },
+  () => console.log("Server started on 5000")
+);
+
+const fetchFortuneCookie = async () => {
+  const response = await fetch(API);
+  const cookie = await response.json();
+  return cookie?.fortune;
+};
+
+wss.on("connection", function connection(ws, req) {
+  console.log("Connection Opened");
+
+  const messageInterval = setInterval(async () => {
+    const data = await fetchFortuneCookie();
+    const cookie = {
+      fortune: data,
+      id: Date.now(),
+      date: Date.now(),
+    };
+
+    ws.send(JSON.stringify(cookie));
+  }, 5000);
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+    clearInterval(messageInterval);
+  });
 });
